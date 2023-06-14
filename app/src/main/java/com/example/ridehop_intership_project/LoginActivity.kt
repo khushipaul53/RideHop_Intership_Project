2fea28fed0152126ebe25ad702693049125f7dc5
khushipaul53 <khushipaul53@gmail.com>
2023-06-13 23:32:19 -0400

    splash and login screen ui done

---
+++ app/src/main/java/com/example/ridehop_intership_project/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.ridehop_intership_project
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.SignupActivity
+import com.example.ridehop_intership_project.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+       var binding:  ActivityLoginBinding?=null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+
+        binding!!.btLogin!!.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(applicationContext, HomeActivity::class.java))
+
+        })
+
+        binding!!.TvSignup!!.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(applicationContext, SignupActivity::class.java))
+
+        })
+
+
+    }
+}
---
+++ app/src/main/java/com/example/ridehop_intership_project/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ridehop_intership_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.databinding.DataBindingUtil
+import java.util.*
+
+class SplashActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(applicationContext, LoginActivity::class.java))
+            }
+        }, 3000)
+    }
+
+    }
+
